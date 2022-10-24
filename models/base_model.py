9205138e2360b78c68e82c673bf92e54ffc8118e
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-24 17:45:58 -0500

    [TASK 3, FEATURES]: add d in the updated attribute

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel:
         """"initialize variables and methods"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """
@@ -28,7 +28,7 @@ class BaseModel:
         method that updates the public instance attribute updated_at
         with the current datetime
         """
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
@@ -37,6 +37,6 @@ class BaseModel:
         dict_return = self.__dict__.copy()
         dict_return["__class__"] = self.__class__.__name__
         dict_return["created_at"] = self.created_at.isoformat()
-        dict_return["updated_at"] = self.update_at.isoformat()
+        dict_return["updated_at"] = self.updated_at.isoformat()
 
         return dict_return
