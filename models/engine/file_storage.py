f29e0acc85c7b31b61c77b3d2d44c1e828ee672c
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-25 15:27:56 -0500

    corection bug identation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,7 @@ FileStorage class module
 import json
 import os.path
 
+
 class FileStorage:
     """
     FileStorage class module
@@ -38,6 +39,5 @@ class FileStorage:
         if os.path.exists(self.__file_path):
             with open(self.__file_path, "r", encoding="utf-8") as file:
                 json_object = json.loads(file.read())
-
-                for key, value in json_object.items():
-                    self.__objects[key] = eval(value["__class__"])(**value)
+            for key, value in json_object.items():
+                self.__objects[key] = eval(value["__class__"])(**value)
