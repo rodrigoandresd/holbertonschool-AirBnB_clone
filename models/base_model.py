a43ecaccd9dca399705322873e164206e7e274f5
rodrigoandresd <rodrigoaduqueg@gmail.com>
2022-10-25 15:21:17 -0500

    update basemodel file_storage

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 """
 from datetime import datetime
 import uuid
+from models import storage
 
 
 class BaseModel:
@@ -19,8 +20,9 @@ class BaseModel:
                 setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.today()
-            self.updated_at = datetime.today()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
@@ -37,6 +39,7 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 serializes object/instances to a JSON file and deserializes
 JSON file to objects/instances
@@ -20,20 +21,25 @@ class FileStorage:
     def all(self):
         """ Returns the dictionaty __objects """
         return self.__objects
-    
+
     def new(self, obj):
         """ Sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        key = obj.__class__.__name__, obj.id
         self.__objects[key] = obj
-    
+
     def save(self):
         """ serializes __objects to the JSON file """
         dict_json = {}
         for key, value in self.__objects.items():
             dict_json[key] = value.to_dict()
-        with open(self.__file_path, "w") as file:
+        with open(self.__file_path, "w", encoding="utf-8") as file:
             file.write(json.dumps(dict_json))
 
     def reload(self):
         """  Deserializes the JSON file to __objects """
-    
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, "r", encoding="utf-8") as file:
+                json_object = json.loads(file.read())
+
+                for key, value in json_object.items():
+                    self.__objects[key] = eval(value["__class__"])(**value)
