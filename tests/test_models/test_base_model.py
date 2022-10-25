e1b1cb2ff88eba997ee12aa80822a8ea7012cb80
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-24 21:00:51 -0500

    [TASK 3, TEST FILE Base model]: add all Test case

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python3
 """
 """
+from datetime import datetime
+from models.base_model import BaseModel
 import unittest
 
 
-class TestBaseModel(unittest.TestCases):
-    pass
+class TestBaseModel(unittest.TestCase):
+
+    def test_id(self):
+        """Test Id attribute"""
+        _id = type(BaseModel().id)
+        self.assertEqual(str, _id)
+
+    def test_currented_at(self):
+        """Test currented_at attribute"""
+        date = datetime
+        self.assertEqual(date, type(BaseModel().created_at))
+
+    def test_updated_at(self):
+        """Test updated_at attribute"""
+        date = datetime
+        self.assertEqual(date, type(BaseModel().updated_at))
+
+    def test_to_dict(self):
+        """Test to_dict method"""
+        base = BaseModel()
+        self.assertEqual(base.to_dict()['__class__'], base.__class__.__name__)
+
+    def test_str(self):
+        """ Test __str__ method """
+        base = BaseModel().__str__()
+        _str = BaseModel().__str__()
+        self.assertNotEqual(_str, base)
 
 
 if __name__ == "__main__":
