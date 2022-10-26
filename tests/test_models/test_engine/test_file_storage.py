4e0ee7699630a7d4c06611c05a7e33bad1ab0b9e
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-25 19:30:05 -0500

    add nnew test for all method

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,19 +3,15 @@
 Test file for File Storage
 """
 import unittest
-
-import os
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from os import path
 
 
-
 class TestFileStorage(unittest.TestCase):
     """"""
-
 
     def test_all_method(self):
         storage = FileStorage()
         self.assertEqual(type(storage.all()), dict)
-
+        self.assertIs(storage.all(), storage._FileStorage__objects)
