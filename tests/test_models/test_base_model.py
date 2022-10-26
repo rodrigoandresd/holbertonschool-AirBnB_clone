9c3a009f9948b619d5b13427884a72b50cd196f6
Luiyi_FHB <luiyifernando5@gmail.com>
2022-10-26 16:15:40 -0500

    [TASK 3, TEST FILE]: fixed  save test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -25,9 +25,9 @@ class TestBaseModel(unittest.TestCase):
 
     def test_save(self):
         """Test save updated_at"""
-        self.BaseModel().save()
-        self.assertNotEqual(self.BaseModel().created_at,
-                            self.BaseModel().updated_at)
+        self.BaseModel.save()
+        self.assertNotEqual(self.BaseModel.created_at,
+                            self.BaseModel.updated_at)
 
     def test_to_dict(self):
         """Test to_dict method"""
